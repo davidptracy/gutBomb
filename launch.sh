314b3943e6c4ed1f856287625c532b1dcd0b3d44
SEPA Kiosk <sepakiosk@SEPAs-Mac-mini.local>
2015-12-07 23:44:37 -0500

    launch shell almost working

---
+++ launch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sleep 10
+echo "*********** AMNH SEPA Kiosk Startup Script ***********"
+echo "Starting Database ... "
+cd /Users/sepakiosk/Documents/sepaKioskApp/
+/usr/local/Cellar/mongodb/3.0.7/bin/mongod --fork --logpath mongoLog.log --dbpath data
+# mongod --fork --logpath /Users/sepakiosk/Documents/sepaKioskApp/mongoLogs/mongod.log --dbpath /Users/sepakiosk/Documents/sepaKioskApp/data
+echo "Waiting 5 seconds ..."
+sleep 5
+echo "Starting Node.js server ... "
+# launchctl load /Users/sepakiosk/Documents/sepaKioskApp/launchAgents/com.amnh.nodeLaunch.plist
+cd /Users/sepakiosk/Documents/sepaKioskApp/
+/usr/local/bin/forever start server.js
+echo "Waiting 5 seconds ..."
+sleep 5
+echo "Starting Launch Agent for Google Chrome"
+launchctl load /Users/sepakiosk/Documents/sepaKioskApp/launchAgents/com.amnh.chromeLauncher.plist
