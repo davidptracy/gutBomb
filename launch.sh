b51db224c17f2e11d547acfd25e0fba9050a1bff
SEPA Kiosk <sepakiosk@SEPAs-Mac-mini.local>
2015-12-08 23:18:05 -0500

    added extra path for log directory

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ sleep 10
 echo "*********** AMNH SEPA Kiosk Startup Script ***********"
 echo "Starting Database ... "
 cd /Users/sepakiosk/Documents/sepaKioskApp/
-/usr/local/Cellar/mongodb/3.0.7/bin/mongod --fork --logpath mongoLog.log --dbpath data
+/usr/local/Cellar/mongodb/3.0.7/bin/mongod --fork --logpath mongoLogs/mongoLog.log --dbpath data
 # mongod --fork --logpath /Users/sepakiosk/Documents/sepaKioskApp/mongoLogs/mongod.log --dbpath /Users/sepakiosk/Documents/sepaKioskApp/data
 echo "Waiting 5 seconds ..."
 sleep 5
